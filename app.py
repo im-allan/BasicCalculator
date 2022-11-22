ce6b51ec630495e8d1784f3ae0baa63f64c7e73e
Allan <al.silvaquiroz@gmail.com>
2022-11-21 22:07:33 -0300

    numbers function

--- app.py
+++ app.py
@@ -52,6 +52,9 @@ class MainApplication(tk.Frame):
                 display.insert(MainApplication.i, operator)
                 MainApplication.i+=operator_length
         
+        def get_number(n):
+                display.insert(MainApplication.i, n)
+                MainApplication.i+=1
 
 
 
@@ -67,25 +70,25 @@ class MainApplication(tk.Frame):
         tk.Button(frame, text = "²√×", borderwidth=0, command=lambda: get_operation("²√×")).grid(row = 4, column = 6, columnspan=3, sticky="news")
         tk.Button(frame, text = "÷", borderwidth=0, command=lambda: get_operation("÷")).grid(row = 4, column = 9, columnspan=3, sticky="news")
 
-        tk.Button(frame, text = "7", borderwidth=0).grid(row = 5, column = 0, columnspan=3, sticky="news")
-        tk.Button(frame, text = "8", borderwidth=0).grid(row = 5, column = 3, columnspan=3, sticky="news")
-        tk.Button(frame, text = "9", borderwidth=0).grid(row = 5, column = 6, columnspan=3, sticky="news")
+        tk.Button(frame, text = "7", borderwidth=0, command=lambda: get_number(7)).grid(row = 5, column = 0, columnspan=3, sticky="news")
+        tk.Button(frame, text = "8", borderwidth=0, command=lambda: get_number(8)).grid(row = 5, column = 3, columnspan=3, sticky="news")
+        tk.Button(frame, text = "9", borderwidth=0, command=lambda: get_number(9)).grid(row = 5, column = 6, columnspan=3, sticky="news")
         tk.Button(frame, text = "⨉", borderwidth=0).grid(row = 5, column = 9, columnspan=3, sticky="news")
 
 # # --------------- Arithmetic Buttons -------------------------------------------
 
-        tk.Button(frame, text = "4", borderwidth=0).grid(row = 5, column = 0, columnspan=3, sticky="news")
-        tk.Button(frame, text = "5", borderwidth=0).grid(row = 5, column = 3, columnspan=3, sticky="news")
-        tk.Button(frame, text = "6", borderwidth=0).grid(row = 5, column = 6, columnspan=3, sticky="news")
+        tk.Button(frame, text = "4", borderwidth=0, command=lambda: get_number(4)).grid(row = 5, column = 0, columnspan=3, sticky="news")
+        tk.Button(frame, text = "5", borderwidth=0, command=lambda: get_number(5)).grid(row = 5, column = 3, columnspan=3, sticky="news")
+        tk.Button(frame, text = "6", borderwidth=0, command=lambda: get_number(6)).grid(row = 5, column = 6, columnspan=3, sticky="news")
         tk.Button(frame, text = "-", borderwidth=0).grid(row = 5, column = 9, columnspan=3, sticky="news")
 
-        tk.Button(frame, text = "1", borderwidth=0).grid(row = 6, column = 0, columnspan=3, sticky="news")
-        tk.Button(frame, text = "2", borderwidth=0).grid(row = 6, column = 3, columnspan=3, sticky="news")
-        tk.Button(frame, text = "3", borderwidth=0).grid(row = 6, column = 6, columnspan=3, sticky="news")
+        tk.Button(frame, text = "1", borderwidth=0, command=lambda: get_number(1)).grid(row = 6, column = 0, columnspan=3, sticky="news")
+        tk.Button(frame, text = "2", borderwidth=0, command=lambda: get_number(2)).grid(row = 6, column = 3, columnspan=3, sticky="news")
+        tk.Button(frame, text = "3", borderwidth=0, command=lambda: get_number(3)).grid(row = 6, column = 6, columnspan=3, sticky="news")
         tk.Button(frame, text = "+", borderwidth=0).grid(row = 6, column = 9, columnspan=3, sticky="news")
 
         tk.Button(frame, text = "±", borderwidth=0).grid(row = 7, column = 0, columnspan=3, sticky="news")
-        tk.Button(frame, text = "0", borderwidth=0).grid(row = 7, column = 3, columnspan=3, sticky="news")
+        tk.Button(frame, text = "0", borderwidth=0, command=lambda: get_number(0)).grid(row = 7, column = 3, columnspan=3, sticky="news")
         tk.Button(frame, text = ",", borderwidth=0).grid(row = 7, column = 6, columnspan=3, sticky="news")
         tk.Button(frame, text = "=", borderwidth=0).grid(row = 7, column = 9, columnspan=3, sticky="news")
 
